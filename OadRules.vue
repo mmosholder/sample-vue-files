746a2777d4e392b6836c9fad05e43ae1e442da25
Malinda Mosholder <m.m.mosholder@gmail.com>
2019-04-09 22:39:42 -0600

    pull out block of text for easier readability

---
+++ OadRules.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="xs-pr-15 xs-pl-15 sm-pl-0 sm-pr-0">
+    <div class="col-12 col-md-8 xs-mt-30">
+      <h2 class="title-4 text-underline text-dark xs-mb-20">OAD Guide</h2>
+      <p class="text-18 text-dark">Welcome to the 2019 Gup's Corner One And Done -- we are so glad you are part of the community. We wanted to be sure you were set with our
+        brand new picking interface, so hopefully all of your questions can be answered below. If not, feel free to reach out to <a href="https://twitter.com/GWBuffalo7">@GWBuffalo7</a> or <a href="https://twitter.com/BeerAndBlank">@BeerAndBlank</a>
+        on Twitter, or fire an email out to jbpowerranking@gmail.com with any questions or concerns.
+      </p>
+      <p class="xs-mt-20"><a href="/downloads/2019-PGA-OAD-Rules.pdf" class="text-22-bold no-decoration text-teal link-teal" target="_blank">OAD Rules</a></p>
+      <p class="xs-mt-20 xs-mb-10 text-22-bold text-teal">Are my picks saved?</p>
+      <p class="text-18 text-dark">Right off the bat, we wanted to be very clear about whether or not your picks have been saved to the database. You'll (hopefully) notice some text under your fancy cursive team name -- here's where you see if your picks are saved or not. When you do save a previously un-picked entry, you'll see a small success message
+      at the top of your screen, and the saved/not saves text should show 'saved' and turn green immediately.</p>
+      <p class="xs-mt-20 xs-mb-10 text-22-bold text-teal">Can I accidentally pick a previously used golfer?</p>
+      <p class="text-18 text-dark"><strong>Definitely not. </strong>However, if you feel there is an error in the list of golfers being presented to you for picking from, please get in touch with us and we'll resolve the issue quickly.
+      If you do happen to try to save a golfer in your list who you have taken before, you will be shown an error message and the pick will not be saved.</p>
+    </div>
+  </div>
+</template>
